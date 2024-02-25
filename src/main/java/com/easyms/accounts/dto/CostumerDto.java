fa4d90744afb9cff9d8303b719671dfc655680d3
Dev Pedro Azevedo <pedroazevedoteixeira7@gmail.com>
2024-02-24 23:40:17 -0300

    Added DTO's and error treatments

---
+++ src/main/java/com/easyms/accounts/dto/CostumerDto.java
@@ -0,0 +1,14 @@
+package com.easyms.accounts.dto;
+
+import lombok.Data;
+
+@Data
+public class CostumerDto {
+
+    private String name;
+
+    private String email;
+
+    private String mobileNumber;
+
+}
--- src/main/java/com/easyms/accounts/entity/Accounts.java
+++ src/main/java/com/easyms/accounts/entity/Accounts.java
@@ -1,8 +1,6 @@
 package com.easyms.accounts.entity;
 
 import jakarta.persistence.Column;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.Data;
 
